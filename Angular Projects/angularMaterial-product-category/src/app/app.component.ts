a6e0581d1da4c427b66af338ad82dc4f3547f489
yorkmacbook058 <lanescheffler@gmail.com>
2022-07-27 13:48:11 -0500

    commit 3.0 : final product - added filter, edit, delete to finish crud ops, minor css styling

--- Angular Projects/angularMaterial-product-category/src/app/app.component.ts
+++ Angular Projects/angularMaterial-product-category/src/app/app.component.ts
@@ -1,48 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogModule, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { ApiService } from './services/api.service';
 
-import {AfterViewInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'angularMaterial-product-category';
 
-  displayedColumns: string[] = ['productName', 'category', 'price', 'freshness','date', 'comments'];
+  displayedColumns: string[] = [
+    'productName',
+    'category',
+    'price',
+    'freshness',
+    'date',
+    'comments',
+    'action',
+  ];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private dialog : MatDialog, private api : ApiService) {
+  constructor(private dialog: MatDialog, private api: ApiService) {}
 
-  }
   ngOnInit(): void {
     this.getAllProducts();
   }
+
   openDialog() {
-    this.dialog.open(DialogComponent, {
-      width: '30%'
-    });
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getAllProducts();
+        }
+      })
   }
+
   getAllProducts() {
-    this.api.getProduct()
-    .subscribe({
+    this.api.getProduct().subscribe({
       next: (res) => {
-       this.dataSource = new MatTableDataSource(res);
-       this.dataSource.paginator = this.paginator;
-       this.dataSource.sort = this.sort
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: (err) => {
-        alert('Error while fetching records!')
+        alert('Error while fetching records!');
+      },
+    })
+  }
+
+  editProduct(row: any) {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row,
+    }).afterClosed().subscribe(val => {
+      if(val === 'update') {
+        this.getAllProducts();
+      }
+    })
+  }
+
+  deleteProduct(id: number){
+    this.api.deleteProduct(id).subscribe({
+      next: (res) => {
+        // alert('Product deleted successfully!');
+        this.getAllProducts();
+      },
+      error: ()=> {
+        alert("Error while deleting product!")
       }
     })
   }
@@ -55,5 +97,4 @@ export class AppComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
 }
